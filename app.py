e9ee84e10d29295fdf030c9bc47c1cda710cc70d
Kamaljeet Kaur <168989813+KK15082005@users.noreply.github.com>
2026-01-08 14:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import  Flask
-
 app = Flask(__name__)
-@app.router("/")
+@app.route("/")
 def home():
     return "PrePilot is LIVE on Azure"
 application = app
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=8000)
+
 
 
 
